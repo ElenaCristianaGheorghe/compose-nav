5fcf2ddb058644c132b7ac0e767aaa2c7e72a7f0
Elena Gheorghe <elena.gheorghe@gendigital.com>
2023-11-15 11:04:33 +0200

    temporary commit

--- app/src/main/java/com/example/myapplication/navigation/HomeNavigation.kt
+++ app/src/main/java/com/example/myapplication/navigation/HomeNavigation.kt
@@ -3,7 +3,11 @@ package com.example.myapplication.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.myapplication.FragmentHolderScreen
 import com.example.myapplication.composables.Home
+import com.example.myapplication.databinding.FrTestBinding
+import com.example.myapplication.databinding.FrTestContainerBinding
+import com.example.myapplication.fragments.TestFragment
 
 fun NavController.navigateToHome() {
     this.navigate(NavDestination.Home.destination)
@@ -16,7 +20,15 @@ fun Navigator.navigateToHome() {
 fun NavGraphBuilder.homeScreen(
 ) {
     composable(route = NavDestination.Home.destination) {
-        Home()
+        homeScreen()
+//        FragmentHolderScreen(
+//            topBarTitle = "Hello",
+//            androidViewBindingFactory = FrTestContainerBinding::inflate,
+//            androidViewBindingUpdate = {
+//                fragmentContainerViewEvents.getFragment<TestFragment>()
+//        },
+//        onBackPress = {},
+//        )
     }
 }
 
